891c76e40b8489a1e2c276513d6fd200b9e61dfe
emsuru <miruna.suru@gmail.com>
2024-02-20 13:37:52 +0100

    added concurrency

--- get-property-conc.py
+++ get-property-conc.py
@@ -1,24 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd # I'm using pandas to save the URLs to a CSV file, as I'm familiar with this package
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 
+# Function to handle the request and parsing for a single page
+def get_links_from_page(base_url, page):
+    search_url = f"{base_url}?countries=BE&page={page}"
+    response = requests.get(search_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        property_links = soup.find_all('a', class_='card__title-link')
+        return [link['href'] for link in property_links if 'href' in link.attrs]
+    else:
+        print(f"Failed to retrieve page {page}: {response.status_code}")
+        return []
 
 # This function retrieves the URLs of the properties listed on the Immoweb search results page
-# When called, it and the list variable
-# It also saves the results to a csv file
-
+# It now uses concurrent requests to speed up the process
 def get_property_links(base_url, pages):
     all_urls = []
-    for page in range(1, pages + 1):
-        search_url = f"{base_url}?countries=BE&page={page}"
-        response = requests.get(search_url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            property_links = soup.find_all('a', class_='card__title-link')
-            urls = [link['href'] for link in property_links if 'href' in link.attrs]
-            all_urls.extend(urls)
-        else:
-            print(f"Failed to retrieve the search results page: {response.status_code}")
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_links_from_page, base_url, page) for page in range(1, pages + 1)]
+        for future in concurrent.futures.as_completed(futures):
+            all_urls.extend(future.result())
     urls_df = pd.DataFrame(all_urls, columns=['URL'])
     urls_df.to_csv('property_urls.csv', index=False)
     return all_urls
@@ -26,7 +31,7 @@ def get_property_links(base_url, pages):
 # Define the base URL for the search results:
 base_search_url = 'https://www.immoweb.be/en/search/house-and-apartment/for-sale'
 # Specify the number of pages we want to scrape: (each search page contains 60 properties)
-total_pages = 10 # 10 pages (600 unique URLS saved) takes 10 seconds
+total_pages = 200 # 10 pages (600 unique URLS saved) takes 10 seconds
 # 30 pages (1800 unique URLS saved) takes 25 seconds
 # 50 pages (3000 unique URLS saved) takes 50 seconds
 # 100 pages (6000 unique URLS saved) takes 90 seconds
---
+++ story.md
@@ -0,0 +1,38 @@
+# Project Diary
+
+
+## Day 1: CHAOS!
+- changing tables, changing chairs, changing everything.
+- Immoweb will be our home for the week
+
+
+- trello? github pages? nothing?
+- settle on a structure first?
+- let's use a generator!
+- already on immoweb, inspecting the html
+- APIs? No APIs? Public, private, paid, hidden?!
+- what verison of Python are we using?
+
+BREAK!
+
+"We have a whole week, we can afford taking a full day to look at this individually and re-group tomorrow morning." "Yes, then we cal all say how we want to approach this." "No, how about working in a team!?"
+
+Aaaaaargh!!!
+
+### Hurdle of the day:
+
+Uh-oh, can't get (some) data from the page, it only loads with the Javascript.
+
+### Successes of the day:
+
+## Day 2
+..
+
+## Day 3
+..
+
+## Day 4
+..
+
+## Day 5
+..
