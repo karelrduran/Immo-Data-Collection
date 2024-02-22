bebcb2bbba6741d3fe234cef0923a0b0897c70a7
Alice Mendes <143808533+Rumineko@users.noreply.github.com>
2024-02-22 14:40:51 +0100

    Created CSV Function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.scraper.data_collection import DataCollector
+from src.scraper.data_output import DataOutput
 
 
 def main():
-    data_collector = DataCollector()
+    DataOutput().to_csv_file("data.csv")
+    """data_collector = DataCollector()
     property_urls = data_collector.get_property_links(100)
     with open("data.json", "r", encoding="utf-8") as file:
         for url in property_urls:
@@ -11,6 +13,7 @@ def main():
             if str(id) not in file.read():
                 data = data_collector.get_data_from_html(url)
                 data_collector.estate_check(data)
+"""
 
 
 if __name__ == "__main__":
--- src/scraper/data_output.py
+++ src/scraper/data_output.py
@@ -1,3 +1,4 @@
+import csv
 import json
 
 
@@ -10,6 +11,12 @@ class DataOutput:
             json.dump(data, file, ensure_ascii=False)
             file.write("\n")
 
-    @classmethod
-    def to_csv_file(cls) -> None:
-        pass
+    def to_csv_file(self, csv_file_path):
+        json_file_path = "data.json"
+        with open(json_file_path, "r") as json_file:
+            data = [json.loads(line) for line in json_file]
+
+        with open(csv_file_path, "w", newline="", encoding="utf-8") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
