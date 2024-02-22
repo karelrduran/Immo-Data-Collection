b9c9a39f20fa405dfbc211c45843005e8bec6458
Alice Mendes <143808533+Rumineko@users.noreply.github.com>
2024-02-22 14:06:38 +0100

    10000 bbyyyy

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from src.scraper.data_collection import DataCollector
 def main():
     data_collector = DataCollector()
     property_urls = data_collector.get_property_links(100)
-    for url in property_urls:
-        data = data_collector.get_data_from_html(url)
-        data_collector.estate_check(data)
+    with open("data.json", "r", encoding="utf-8") as file:
+        for url in property_urls:
+            id = url.split("/")[-1]
+            print(url)
+            if str(id) not in file.read():
+                data = data_collector.get_data_from_html(url)
+                data_collector.estate_check(data)
 
 
 if __name__ == "__main__":
