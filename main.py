64a00487eab73cdd79b939cc8885cb1f258fff1e
Rumineko <143808533+Rumineko@users.noreply.github.com>
2024-03-04 07:10:46 +0100

    Fixed duplicates

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ from src.scraper.data_output import DataOutput
 
 def main():
     data_collector = DataCollector()
-    property_urls = data_collector.get_property_links(2)
-    with open(os.path.join("data", "data.json"), "r", encoding="utf-8") as file:
+    property_urls = data_collector.get_property_links(50)
+    with open(
+        os.path.join("data", "already_parsed.json"), "a", encoding="utf-8"
+    ) as file:
         for url in property_urls:
             id_ = url.split("/")[-1]
             print(url)
-            if str(id_) not in file.read():
-                data = data_collector.get_data_from_html(url)
-                data_collector.estate_check(data)
+            if id_ in file.read():
+                if data_collector.check_existant(id_) == False:
+                    print("new property found")
+                    file.write(id_ + "\n")
+                    data = data_collector.get_data_from_html(url)
+                    data_collector.estate_check(data)
+            else:
+                print("property already exists")
+                file.write(id_ + "\n")
     DataOutput().to_csv_file(os.path.join("data", "data.csv"))
 
 
